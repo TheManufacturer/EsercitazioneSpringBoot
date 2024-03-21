fc43e7d93e5ed7d1634a773910bf5b7b7e95bc02
TheManufacturer <antonio.mario.demarco@gmail.com>
2024-03-21 11:06:37 +0100

    OK

---
+++ EsercizioUploadUltimoQui/EsercizioUploadDownload/src/test/java/com/example/EsercizioUploadDownload/EsercizioUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EsercizioUploadDownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioUploadDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
